717a6db645833711ae23c299260e090561e1b5cc
teguhrachmadi1206-png <teguh.rachmadi.1206@gmail.com>
2026-02-13 23:19:28 +0700

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/teguhrachmadi1206-png/Al-Quran-Random-Ayah-Generator-v1.3'
+  base: '/Al-Quran-Random-Ayah-Generator-v1.3'
 })
